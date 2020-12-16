40799e6341e1c3488e539755cd4e6fffac244c47
Sahil Athia <sahilathia918@gmail.com>
2020-12-16 16:21:43 -0500

    add a proxy and .env for testing and development

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,17 @@
 const pg = require("pg");
 
 const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || ""
+  // connectionString: process.env.DATABASE_URL || ""
+  user: process.env.PGUSER || process.env.USER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  database:process.env.PGDATABASE || process.env.USER,
+  port: process.env.PGPORT,
 });
-
+console.log(process.env.DATABASE_URL);
 client
   .connect()
+  .then(() => console.log('connect'))
   .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
 
 module.exports = client;
--- src/routes/days.js
+++ src/routes/days.js
@@ -20,6 +20,7 @@ module.exports = db => {
       response.json(days);
     });
   });
+  
 
   return router;
 };
